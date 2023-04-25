00b67e6cadce4a275ec01bdd990722e4558c8e8a
Pepi100 <radunedelcu02@gmail.com>
2023-04-25 18:16:40 +0300

    Scrolling Background

---
+++ Assets/Flappy Duck/Scripts/BackgroundScroll.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+
+
+
+    public float speed = 1f;
+    private Vector3 StartPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.left*speed*Time.deltaTime);
+        if (transform.position.x < -16f)
+        {
+            transform.position = StartPosition;
+        }
+    }
+}
