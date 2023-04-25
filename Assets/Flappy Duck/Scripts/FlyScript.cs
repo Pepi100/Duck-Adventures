45bf0b278f06b7b5e3a9fb1ae7eee78b82cdab48
Pepi100 <radunedelcu02@gmail.com>
2023-04-25 16:17:45 +0300

    Pipe Prefab

--- Assets/Flappy Duck/Scripts/FlyScript.cs
+++ Assets/Flappy Duck/Scripts/FlyScript.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FlyScript : MonoBehaviour
 {
 
     public Rigidbody2D rb;
     public float velocity = 1;
+    public TextMeshProUGUI scoreText;
+
+    private int score;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        score = 0;
         rb.velocity = Vector2.up * velocity;
 
     }
@@ -20,7 +26,7 @@ public class FlyScript : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetMouseButtonDown(0) )
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown("space"))
         {
             //jump
             rb.velocity = Vector2.up * velocity;
@@ -30,6 +36,26 @@ public class FlyScript : MonoBehaviour
 
 
 
-        
+
+    }
+
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("ScoreTrigger"))
+        {
+            score++;
+            scoreText.text = score.ToString();
+        }
+
+
+
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        scoreText.text = "Ai murit, fraiere";
     }
 }
