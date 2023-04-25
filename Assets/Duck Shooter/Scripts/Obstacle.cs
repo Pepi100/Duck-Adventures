f67e1795d9cec73db5ab6d656ab8ce4280c0991c
iosoicristiana <niculitacristiana@gmail.com>
2023-04-25 18:14:17 +0300

    main si settings menu aproape complet

--- Assets/Duck Shooter/Scripts/Obstacle.cs
+++ Assets/Duck Shooter/Scripts/Obstacle.cs
@@ -4,7 +4,7 @@ public class Obstacle : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        if(other.gameObject.layer == LayerMask.NameToLayer("Default"))
         {
             this.gameObject.SetActive(false);
             Debug.Log("destroyed obstacles");
--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class SettingsMenu : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
+    public float volumevalue;
 
+    void Start()
+    {
+        audioMixer = PlayerPrefs.GetFloat("Volume");
+    }
+    void Update()
+    {
+        audioMixer.SetFloat("Volume", volumevalue);
+        PlayerPrefs.SetFloat("Volume", volumevalue);
+        PlayerPrefs.Save();
+    }
     public void SetVolume(float volume)
     {
-        audioMixer.SetFloat("volume", volume);
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene("Main Menu");
     }
 }
