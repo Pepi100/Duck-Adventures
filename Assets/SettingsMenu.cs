fe21b14ea1372c19a9f231e591299ef36e92e478
iosoicristiana <niculitacristiana@gmail.com>
2023-05-02 22:39:16 +0300

    fixed main menu + settings + added working volume

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -1,30 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
+
 public class SettingsMenu : MonoBehaviour
 {
-
-    public AudioMixer audioMixer;
-    public float volumevalue;
-
-    void Start()
-    {
-        audioMixer = PlayerPrefs.GetFloat("Volume");
-    }
-    void Update()
-    {
-        audioMixer.SetFloat("Volume", volumevalue);
-        PlayerPrefs.SetFloat("Volume", volumevalue);
-        PlayerPrefs.Save();
-    }
-    public void SetVolume(float volume)
-    {
-        audioMixer.SetFloat("Volume", volume);
-    }
 
     public void GoBack()
     {
